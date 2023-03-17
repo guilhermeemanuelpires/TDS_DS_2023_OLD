c69c51a67e79289ef157013ace47850dd1d84e00
Guilherme Pires <guilhermeemanuel45@gmail.com>
2023-03-17 11:50:04 -0300

    aula 16/03

--- lista_mercado/script.js
+++ lista_mercado/script.js
@@ -1,14 +1,21 @@
 const input = window.document.getElementById("input");
 const ul = window.document.querySelector("ul");
+const db_lista_li = JSON.parse(window.localStorage.getItem("lista"));
 
-function adicionar() {
+db_lista_li.forEach((item) => adicionar(item));
+
+function adicionar(item) {
   var li = window.document.createElement("li");
-  var value = input.value;
+  var value = item ? item.texto : input.value;
   var value_li = window.document.createTextNode(value);
 
-  if (input.value.trim() !== "") {
+  if (input.value.trim() !== "" || item) {
     li.appendChild(value_li);
     ul.appendChild(li);
+  }
+
+  if (!item) {
+    adiciona_lista();
   }
 
   input.value = "";
@@ -29,5 +36,16 @@ window.document.addEventListener("contextmenu", (e) => {
   e.preventDefault();
   if (e.target.tagName === "LI") {
     e.target.remove();
+    adiciona_lista();
   }
 });
+
+function adiciona_lista() {
+  const lista_de_li = window.document.querySelectorAll("li");
+  const lista = [];
+  lista_de_li.forEach((item) => {
+    lista.push({ texto: item.innerText });
+  });
+
+  window.localStorage.setItem("lista", JSON.stringify(lista));
+}
