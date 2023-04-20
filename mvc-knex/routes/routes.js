f78e19828265dc98a343608f3cf92aa03bc77615
Guilherme Pires <guilhermeemanuel45@gmail.com>
2023-04-20 13:38:17 -0300

    projeto mvc-knex

--- mvc-knex/routes/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const empresaRoute = require("./empresa.route");
-
-const routes = new express.Router();
-
-routes.get("/empresa", empresaRoute);
-
-module.exports = routes;
